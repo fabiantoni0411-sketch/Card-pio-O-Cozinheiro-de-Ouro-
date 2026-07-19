305cea00af8a023c27ad1f77f7f4f0c3c907402c
fabiantoni0411-sketch <fabiantoni0411@gmail.com>
2026-07-19 00:33:56 -0300

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+// Configuração do Firebase do projeto "Cozinheiro de Ouro v2"
+export const firebaseConfig = {
+  apiKey: "AIzaSyDXAQq1M9deN6rf6O_2yoMLsMU-GxA7Ta4",
+  authDomain: "cozinheiro-de-ouro-v2.firebaseapp.com",
+  projectId: "cozinheiro-de-ouro-v2",
+  storageBucket: "cozinheiro-de-ouro-v2.firebasestorage.app",
+  messagingSenderId: "503813327394",
+  appId: "1:503813327394:web:5fb5bded33ef966d4e587b",
+};
